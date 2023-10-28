47b1d6979b1fc0ba94e25d6c99ea43b27230078b
komsakkonna <mr.kkonna@gmail.com>
2023-10-28 13:50:09 +0700

    Stamped/Unstamped Twist

--- launch/launch_robot.launch.py
+++ launch/launch_robot.launch.py
@@ -29,7 +29,11 @@ def generate_launch_description():
                 )]), launch_arguments={'use_sim_time': 'true','use_ros2_control':'true'}.items()
     )
 
-    
+    # joystick = IncludeLaunchDescription(
+    #             PythonLaunchDescriptionSource([os.path.join(
+    #                 get_package_share_directory(package_name),'launch','jotstick.launch.py'
+    #             )])
+    # )
 
 
     robot_description = Command(['ros2 param get --hide-type /robot_state_publisher robot_description'])
@@ -77,6 +81,7 @@ def generate_launch_description():
     # Launch them all!
     return LaunchDescription([
         rsp,
+        # joystick,
         delayed_controller_manager,
         delayed_diff_drive_spawner,
         delayed_joint_broad_spawner
